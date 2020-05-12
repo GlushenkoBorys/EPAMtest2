bfcdf6824682e6e5af308f74f6ea22f02d2ff682
boris111079@gmail.com <boris111079@gmail.com>
2020-05-12 21:39:09 +0300

    Is done!

--- EPAMtest2/EPAMtest2/Program.cs
+++ EPAMtest2/EPAMtest2/Program.cs
@@ -5,12 +5,34 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace EPAMtest2
-{
+{//2 - вводится строка, в которой вместо пробелов тирэ.
+    //Нужно что бы программа убирала эти тирэ и каждое новое слово начиналось с большой буквы
+ //   Пример: В-городе-началась-весна ( ВГородеНачаласьВесна)
     class Program
     {
         static void Main(string[] args)
         {
-            //
+            Resolve("В-городе-началась-весна");
+        }
+        public static string Resolve(string arg) 
+        {
+            
+            string result = "";
+            char[] arr = arg.ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]=='-')
+                {
+                    result+=arr[i+1].ToString().ToUpper();
+                    i++;
+                }
+                else
+                {
+                    result += arr[i];
+                }
+            }
+
+            return result;
         }
     }
 }
